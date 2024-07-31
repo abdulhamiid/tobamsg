c557325b713af30ec08d15a0762086da656f78f7
abdulhmaiid <adioabdulhamid@yahoo.com>
2024-07-31 17:36:39 +0100

    Add current date generator

---
+++ src/components/CurrentDate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const formatDate = (date) => {
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+  return date.toLocaleDateString('en-GB', options);
+};
+
+const CurrentDate = () => {
+  const today = new Date();
+  return (
+    <div>
+      <p>{formatDate(today)}</p>
+    </div>
+  );
+};
+
+export default CurrentDate;
