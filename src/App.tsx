970c71d7e737c214af51f850ee71160348dfe254
abdulhmaiid <adioabdulhamid@yahoo.com>
2024-08-01 12:56:55 +0100

    Add typescript configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Footer from './components/Footer';
-import Recipe from './pages/Recipe';
+import Navbar from './components/Navbar.js';
+import Home from './pages/Home.js';
+import Footer from './components/Footer.js';
+import Recipe from './pages/Recipe.js';
 
 function App() {
   return (
