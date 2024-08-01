74bb37ea3c97c5bf9d1da3e75714b28829cb387c
abdulhmaiid <adioabdulhamid@yahoo.com>
2024-08-01 06:05:04 +0100

    Add recipe page

---
+++ src/components/Drc.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Drc() {
+  return (
+    <section className='w-[65%] ml-[6%] my-[60px]'>
+        <h3 className='text-3xl font-bold my-6'>Directions</h3>
+        <h4 className='text-lg font-bold'>1. Lorem ipsum dolor sit amet </h4>
+        <div className='px-7 py-4'>
+          <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
+              ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci 
+              velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+          <img className='py-4 w-[100%]' src='Rectangle23.svg' />
+        </div>
+
+
+        <h4 className='text-lg font-bold'>1. Lorem ipsum dolor sit amet </h4>
+        <p className='px-7 py-3'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
+            ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci 
+            velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+        
+            <h4 className='text-lg font-bold'>1. Lorem ipsum dolor sit amet </h4>
+        <p className='px-7 py-3'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
+            ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci 
+            velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+    </section>
+  )
+}
+
+export default Drc
